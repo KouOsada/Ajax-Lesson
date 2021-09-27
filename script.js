fe9a4d171b9635a1427b995fe8f496a833ab2af1
KouOsada <kouosada.0930@gmail.com>
2021-09-27 17:11:49 +0900

    [Add]ajax

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
+const API_KEY = "3d088a0bb700b2ba4ad45246c71c8faa";
 
-$('#tab-menu a').on('click', function(event) {
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("href")).show();
-  event.preventDefault();
+$(function() {
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data) {
+      //通信成功
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt", data.weather[0].main);
+    }).fail(function (data) {
+      //通信失敗
+      alert('通信に失敗しました。');
+    });
+  });
 });
